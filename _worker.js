62477437494f8b87c93f4001ae6ac7c6d1bac8e2
longteng yu <162786720+yuanzi911@users.noreply.github.com>
2024-07-20 18:10:53 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '77a2178a-5862-4e2a-8982-77b6a071d416';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
